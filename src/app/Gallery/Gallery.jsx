186426cc89079eb5e9487b42162f0e9a070128e4
Erblin Krasniqi <erblini.kr@gmail.com>
2023-05-27 23:19:19 +0200

    Added Gallery section

---
+++ src/app/Gallery/Gallery.jsx
@@ -0,0 +1,20 @@
+import classes from "./Gallery.module.scss";
+
+const Gallery = () => {
+  return (
+    <div className={classes.container}>
+      <div>
+        <img src="/1.jpg" alt="" />
+        <img src="/1.jpg" alt="" />
+        <img src="/1.jpg" alt="" />
+        <img src="/1.jpg" alt="" />
+        <img src="/1.jpg" alt="" />
+        <img src="/1.jpg" alt="" />
+        <img src="/1.jpg" alt="" />
+        <img src="/1.jpg" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Navigation from "./Navigation/Navigation";
 import Background from "./Navigation/Background";
 import Description from "./Description/Description";
 import Modules from "./Modules/Modules";
+import Gallery from "./Gallery/Gallery";
 import { Fragment } from "react";
 
 export default function page() {
@@ -12,6 +13,7 @@ export default function page() {
       <Background />
       <Description />
       <Modules />
+      <Gallery />
     </Fragment>
   );
 }
