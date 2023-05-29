7dd95ce8eb682a0c90c43fe0bbb1ea52be6b9c05
Erblin Krasniqi <erblini.kr@gmail.com>
2023-05-29 21:26:56 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  experimental: {
+    images: {
+      // Configure your image-related options here
+    },
+  },
+  // Other Next.js configuration options can be added here
+};
